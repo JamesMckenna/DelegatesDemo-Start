50fba4d5e83cf642ac2838ed5d1524272c3d7638
James Mckenna <G.James.Mckenna@hotmail.com>
2020-03-17 14:23:17 -0600

    Add project files.

---
+++ DemoLibrary/ShoppingCartModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class ShoppingCartModel
+    {
+        //The definition for the delegate
+        public delegate void MentionDiscount(decimal subTotal);
+        public List<ProductModel> Items { get; set; } = new List<ProductModel>();
+        
+        //Func is a C# builit in delegate that takes (many - 16, arguments and returns a value - out)
+        //in the use below, we pass in a List of ProductModel, the subTotal and we expect an out (return value).
+        //and we name (give an alias - not too sure is saying alias is correct, but doing so for my understanding) so this Func<T> is easier to use.
+        //I also use the type Action. Also a built in C# Delegate but an Action dosn't have a return type.
+        //Tim Corey mentions that normally, Func and Action would not be used in this way, but this is a Delegate Demo App and so lets show C# Delegates in use.
+        public decimal GenerateTotal(MentionDiscount mentionSubTotal, 
+            Func<List<ProductModel>,decimal,decimal> calculateDiscountedTotal,
+            Action<string> tellUserWeAreDiscounting)
+        {
+            //throw new NotImplementedException();
+
+            //Use delegate. 
+            decimal subTotal = Items.Sum(x => x.Price);
+            mentionSubTotal(subTotal);
+            tellUserWeAreDiscounting("We are applying your discount");
+
+            ////Calculate Discount depending on amount of purchase
+            ////These hard code values are bad practice. It leads to violating the Open Close Principal 
+            //if(subTotal > 100)
+            //{
+            //    return subTotal * 0.80M;
+            //}
+            //else if(subTotal > 50)
+            //{
+            //    return subTotal * 0.85M;
+            //}
+            //else if(subTotal > 10)
+            //{
+            //    return subTotal * 0.90M;
+            //}
+            //else
+            //{
+            //    return subTotal;
+            //}
+
+            decimal total = calculateDiscountedTotal(Items, subTotal);
+
+            return total;
+
+        }
+    }
+}
---
+++ WinFormUI/Dashboard.cs
@@ -0,0 +1,67 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormUI
+{
+    //Opened Dashboard.ca code but right clicking on Dashboard.ca in Solution explorer and selecting "View Code"
+    public partial class Dashboard : Form
+    {
+        ShoppingCartModel cart = new ShoppingCartModel();
+
+        public Dashboard()
+        {
+            InitializeComponent();
+            PopulateCartWithDemoData();
+        }
+
+        private void PopulateCartWithDemoData()
+        {
+            cart.Items.Add(new ProductModel { ItemName = "Cereal", Price = 3.63M });
+            cart.Items.Add(new ProductModel { ItemName = "Milk", Price = 2.95M });
+            cart.Items.Add(new ProductModel { ItemName = "Strawberries", Price = 7.51M });
+            cart.Items.Add(new ProductModel { ItemName = "Blueberries", Price = 8.84M });
+        }
+
+        private void messageBoxDemoButton_Click(object sender, EventArgs e)
+        {
+            decimal total = cart.GenerateTotal(SubTotalAlert, CalcualteLeveledDiscount, PrintOutDiscountAlert);
+
+            MessageBox.Show($"The total is {total:C2}");
+        }
+
+
+        //This is a event and it's wired up as a delegate (Observer Pattern)
+        private void textBoxDemoButton_Click(object sender, EventArgs e)
+        {
+            decimal total = cart.GenerateTotal((subTotal) => subTotalTextBox.Text = $"{subTotal:C2}",
+                (products, subTotal) => subTotal - (products.Count), /*this inline delegate returns a value. That return value comes from ShoppingCartModel.GenerateTotal method*/
+                (message) => {/*runs method, but doesn't do anything*/ });
+
+            totalTextBox.Text = $"{total:C2}";
+        }
+
+        private void PrintOutDiscountAlert(string message)
+        {
+            // Console.WriteLine really only works in the Console App, doesn't work in WinForm apps unless run in Debug mode. 
+            MessageBox.Show(message);
+        }
+
+        private void SubTotalAlert(decimal subTotal)
+        {
+            MessageBox.Show($"The subtotal is {subTotal:C2}");
+        }
+
+        private decimal CalcualteLeveledDiscount(List<ProductModel> products, decimal subTotal)
+        {
+                return subTotal - products.Count; //BAD Business logic....but this is a demo
+        }
+    }
+}
